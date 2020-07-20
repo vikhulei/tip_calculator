37c6268d94d1f0a0a7df86f579db76c678f7db77
vikhulei <vikhulei@users.noreply.github.com>
2020-07-20 16:29:40 +0300

    with_js

--- tip.js
+++ tip.js
@@ -1,40 +1,60 @@
+bill.addEventListener('focusout', function(e) {
+  var num = Number(bill.value)*Number(tip.value)/100/Number(people.value);
+  result_tip.innerHTML = '$' + num.toFixed(2);
+  var num = Number(bill.value)*Number(tip.value)/100/Number(people.value) + Number(bill.value);
+  result_total.innerHTML = '$' + num.toFixed(2);
+  e.preventDefault()
+}, false)
+
 tip_minus.addEventListener('click', function(e) {
    tip.value = document.getElementById("tip").value - 1;
-   result_tip.innerHTML = Number(bill.value)*Number(tip.value)/100/Number(people.value);
-   result_total.innerHTML = Number(bill.value)+Number(bill.value)*Number(tip.value)/100/Number(people.value);
+   var num = Number(bill.value)*Number(tip.value)/100/Number(people.value);
+  result_tip.innerHTML = '$' + num.toFixed(2);
+  var num = Number(bill.value)*Number(tip.value)/100/Number(people.value) + Number(bill.value);
+  result_total.innerHTML = '$' + num.toFixed(2);
    e.preventDefault()
 }, false)
 
 tip_plus.addEventListener('click', function(e) {
    tip.value = Number(document.getElementById("tip").value) + 1;
-   result_tip.innerHTML = Number(bill.value)*Number(tip.value)/100/Number(people.value);
-   result_total.innerHTML = Number(bill.value)+Number(bill.value)*Number(tip.value)/100/Number(people.value); 
+   var num = Number(bill.value)*Number(tip.value)/100/Number(people.value);
+  result_tip.innerHTML = '$' + num.toFixed(2);
+  var num = Number(bill.value)*Number(tip.value)/100/Number(people.value) + Number(bill.value);
+  result_total.innerHTML = '$' + num.toFixed(2);
+   e.preventDefault()
+ }, false)
+
+ tip.addEventListener('focusout', function(e) {
+  var num = Number(bill.value)*Number(tip.value)/100/Number(people.value);
+  result_tip.innerHTML = '$' + num.toFixed(2);
+  var num = Number(bill.value)*Number(tip.value)/100/Number(people.value) + Number(bill.value);
+  result_total.innerHTML = '$' + num.toFixed(2);
    e.preventDefault()
  }, false)
 
  people_minus.addEventListener('click', function(e) {
    people.value = document.getElementById("people").value - 1;
-   result_tip.innerHTML = Number(bill.value)*Number(tip.value)/100/Number(people.value);
-   result_total.innerHTML = Number(bill.value)+Number(bill.value)*Number(tip.value)/100/Number(people.value);
+   var num = Number(bill.value)*Number(tip.value)/100/Number(people.value);
+   result_tip.innerHTML = '$' + num.toFixed(2);
+   var num = Number(bill.value)*Number(tip.value)/100/Number(people.value) + Number(bill.value);
+   result_total.innerHTML = '$' + num.toFixed(2);
    e.preventDefault()
 }, false)
 
 people_plus.addEventListener('click', function(e) {
     people.value = Number(document.getElementById("people").value) + 1;
-    result_tip.innerHTML = Number(bill.value)*Number(tip.value)/100/Number(people.value);
-    result_total.innerHTML = Number(bill.value)+Number(bill.value)*Number(tip.value)/100/Number(people.value);
+    var num = Number(bill.value)*Number(tip.value)/100/Number(people.value);
+    result_tip.innerHTML = '$' + num.toFixed(2);
+    var num = Number(bill.value)*Number(tip.value)/100/Number(people.value) + Number(bill.value);
+    result_total.innerHTML = '$' + num.toFixed(2);
     e.preventDefault()
  }, false)
 
- bill.addEventListener('focusout', function(e) {
-   result_tip.innerHTML = '$' + Number(bill.value)*Number(tip.value)/100/Number(people.value);
-   result_total.innerHTML = '$' + Number(bill.value)+Number(bill.value)*Number(tip.value)/100/Number(people.value);
-   e.preventDefault()
- }, false)
 
- tip.addEventListener('focusout', function(e) {
-   result_tip.innerHTML = Number(bill.value)*Number(tip.value)/100/Number(people.value);
-   result_total.innerHTML = Number(bill.value)+Number(bill.value)*Number(tip.value)/100/Number(people.value);
+ people.addEventListener('focusout', function(e) {
+  var num = Number(bill.value)*Number(tip.value)/100/Number(people.value);
+  result_tip.innerHTML = '$' + num.toFixed(2);
+  var num = Number(bill.value)*Number(tip.value)/100/Number(people.value) + Number(bill.value);
+  result_total.innerHTML = '$' + num.toFixed(2);
    e.preventDefault()
  }, false)
- 
