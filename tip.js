a9ea1f33ae6df03943ebbfec2b1776cc6547141e
vikhulei <vikhulei@users.noreply.github.com>
2020-07-26 10:13:04 +0300

    functions separately

--- tip.js
+++ tip.js
@@ -1,93 +1,57 @@
-bill.addEventListener('focusout', function(e) {
-  var num = Number(bill.value)*Number(tip.value)/100/Number(people.value);
-  result_tip.innerHTML = '$' + num.toFixed(2);
-  var num = (Number(bill.value)*Number(tip.value)/100 + Number(bill.value))/Number(people.value);
-  result_total.innerHTML = '$' + num.toFixed(2);
-   e.preventDefault()
-}, false)
+function function_calc() {
+  var resultTip = Number(bill.value)*Number(tip.value)/100/Number(people.value);
+  result_tip.innerHTML = '$' + resultTip.toFixed(2);
+  var resultTotal = (Number(bill.value)*Number(tip.value)/100 + Number(bill.value))/Number(people.value);
+  result_total.innerHTML = '$' + resultTotal.toFixed(2);
+}
 
-tip_minus.addEventListener('click', function(e) {
-  tip.value = document.getElementById("tip").value - 1;
-  var num = Number(bill.value)*Number(tip.value)/100/Number(people.value);
-  result_tip.innerHTML = '$' + num.toFixed(2);
-  var num = (Number(bill.value)*Number(tip.value)/100 + Number(bill.value))/Number(people.value);
-  result_total.innerHTML = '$' + num.toFixed(2);
-   e.preventDefault()
-}, false)
+function function_if() {
+if (Number(people.value) > 1) {
+  document.getElementById("sp1").style.visibility = "visible";
+  document.getElementById("sp2").style.visibility = "visible";
+} else if (Number(people.value) === 1) {
+  document.getElementById("sp1").style.visibility = "hidden";
+  document.getElementById("sp2").style.visibility = "hidden";
+}
+}
 
-tip_plus.addEventListener('click', function(e) {
+
+bill.addEventListener('focusout', function f_bill() {
+function_calc();
+})
+
+tip_plus.addEventListener('click', function f_tipPlus() {
   tip.value = Number(document.getElementById("tip").value) + 1;
-  var num = Number(bill.value)*Number(tip.value)/100/Number(people.value);
-  result_tip.innerHTML = '$' + num.toFixed(2);
-  var num = (Number(bill.value)*Number(tip.value)/100 + Number(bill.value))/Number(people.value);
-  result_total.innerHTML = '$' + num.toFixed(2);
-   e.preventDefault()
- }, false)
+  function_calc();
+})
 
- tip.addEventListener('focusout', function(e) {
-  var num = Number(bill.value)*Number(tip.value)/100/Number(people.value);
-  result_tip.innerHTML = '$' + num.toFixed(2);
-  var num = (Number(bill.value)*Number(tip.value)/100 + Number(bill.value))/Number(people.value);
-  result_total.innerHTML = '$' + num.toFixed(2);
-   e.preventDefault()
- }, false)
+tip_minus.addEventListener('click', function f_tipPlus() {
+  tip.value = Number(document.getElementById("tip").value) - 1;
+  function_calc();
+})
 
- people_minus.addEventListener('click', function(e) {
+tip.addEventListener('focusout', function f_tip() {
+  function_calc();
+})
+
+
+people_minus.addEventListener('click', function f_peopleMinus() {
   people.value = document.getElementById("people").value - 1;
-  var num = Number(bill.value)*Number(tip.value)/100/Number(people.value);
-  result_tip.innerHTML = '$' + num.toFixed(2);
-  var num = (Number(bill.value)*Number(tip.value)/100 + Number(bill.value))/Number(people.value);
-  result_total.innerHTML = '$' + num.toFixed(2);
-  if (Number(people.value) > 1) {
-    document.getElementById("sp1").style.visibility = "visible";
-    document.getElementById("sp2").style.visibility = "visible";
-  } else if (Number(people.value) === 1) {
-    document.getElementById("sp1").style.visibility = "hidden";
-    document.getElementById("sp2").style.visibility = "hidden";
-  }
-   e.preventDefault()
-}, false)
+  function_calc();
+  function_if();
+})
 
-people_plus.addEventListener('click', function(e) {
-  people.value = Number(document.getElementById("people").value) + 1;
-  var num = Number(bill.value)*Number(tip.value)/100/Number(people.value);
-  result_tip.innerHTML = '$' + num.toFixed(2);
-  var num = (Number(bill.value)*Number(tip.value)/100 + Number(bill.value))/Number(people.value);
-  result_total.innerHTML = '$' + num.toFixed(2);
-      if (Number(people.value) > 1) {
-        document.getElementById("sp1").style.visibility = "visible";
-        document.getElementById("sp2").style.visibility = "visible";
-      } else if (Number(people.value) === 1) {
-        document.getElementById("sp1").style.visibility = "hidden";
-        document.getElementById("sp2").style.visibility = "hidden";
-      }
-    e.preventDefault()
- }, false)
+people_plus.addEventListener('click', function f_peopePlus() {
+  people.value = document.getElementById("people").value +++1 ;
+  function_calc();
+  function_if();
+})
 
- people.addEventListener('focusout', function(e) {
-  var num = Number(bill.value)*Number(tip.value)/100/Number(people.value);
-  result_tip.innerHTML = '$' + num.toFixed(2);
-  var num = (Number(bill.value)*Number(tip.value)/100 + Number(bill.value))/Number(people.value);
-  result_total.innerHTML = '$' + num.toFixed(2);
-  if (Number(people.value) > 1) {
-    document.getElementById("sp1").style.visibility = "visible";
-    document.getElementById("sp2").style.visibility = "visible";
-  } else if (Number(people.value) === 1) {
-    document.getElementById("sp1").style.visibility = "hidden";
-    document.getElementById("sp2").style.visibility = "hidden";
-  }
-   e.preventDefault()
- }, false)
+people.addEventListener('focusout', function f_peope() {
+  function_calc();
+  function_if();
+})
 
 var form = document.getElementById("tip_calculator");
 function handleForm(event) { event.preventDefault(); } 
 form.addEventListener('submit', handleForm);
-
- /*
- function() {
-  if (Number(people.value) < 1) {
-    alert('');
- 
-  }
-
-} */
